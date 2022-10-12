af400ce01f071c7f5523a046ce9c53c52ef63c0d
emiguelt <edwintriana@gmail.com>
2022-10-12 11:38:58 -0500

    Adding reagent to guestbook prj

--- guestbook/src/cljs/guestbook/core.cljs
+++ guestbook/src/cljs/guestbook/core.cljs
@@ -1,6 +1,31 @@
-(ns guestbook.core)
+(ns guestbook.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as dom]))
 
-(-> (.getElementById js/document "content")
-    (.-innerHTML)
-    (set! "Hi, Hello world"))
+(defn message-form []
+  (let [fields (r/atom {})]
+    (fn []
+      [:div
+       [:div.field
+        [:label.label {:for :name} "Name"]
+        [:input.input
+         {:type      :text
+          :name      :name
+          :on-change #(swap! fields assoc :name (-> % .-target .-value))
+          :value     (:name @fields)}]]
+       [:div.field
+        [:label.label {:for :name} "Message"]
+        [:textarea.textarea
+         {:name      :message
+          :on-change #(swap! fields assoc :message (-> % .-target .-value))
+          :value     (:message @fields)}]]
+       [:input.button.is-primary
+        {:type :submit
+         :value "comment"}]])))
 
+(defn home []
+  [:div.content>div.columns.is-centered>div.columns.is-two-thirds
+   [:div.columns>div.column
+    [message-form]]])
+
+(dom/render [home] (.getElementById js/document "content"))
